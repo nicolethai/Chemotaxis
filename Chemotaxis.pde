11183c0fcd24eda6fa025c0d2409b01f47ab9db4
nicolethai <ThaiNicole@gmail.com>
2015-09-17 11:24:28 -0700

    position and shapes

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -5,10 +5,11 @@ Bacteria [] colony = new Bacteria[NUM_BACTERIA];
  void setup()   
  {     
  	size(500, 500);
+ 	frameRate(100);
  	//initialize bacteria variables here
  	for (int i = 0; i < colony.length; i++)
  	{
- 		colony[i] = new Bacteria(250, 250);
+ 		colony[i] = new Bacteria(250, 250); //(int)(Math.random()*500), (int)(Math.random()*500)
  	} 
  }
 
@@ -21,11 +22,6 @@ Bacteria [] colony = new Bacteria[NUM_BACTERIA];
  		colony[i].move();
  		colony[i].show();
  	}   
-}
-
-void mouseClicked()
-{
-	// NUM_BACTERIA = NUM_BACTERIA*2;
 }
 
 class Bacteria    
@@ -44,15 +40,32 @@ class Bacteria
  	}   
  	void show()
  	{
- 		randFill = (int)(Math.random()*3);
- 		if (randFill == 0)
- 			fill(27,247, 201);
- 		else if (randFill == 1)
- 			fill(252, 0, 152);
- 		else 
- 			fill(255, 102, 0);
- 		noStroke();
- 		// fill(255);
- 		ellipse(xPos, yPos, 5, 5);
+ 		int randShape = (int)(Math.random()*2);
+ 		if (randShape == 0)
+ 		{
+	 		randFill = (int)(Math.random()*3);
+	 		if (randFill == 0)
+	 			fill(27,247, 201);
+	 		else if (randFill == 1)
+	 			fill(252, 0, 152);
+	 		else 
+	 			fill(255, 102, 0);
+	 		noStroke();
+	 		// fill(255);
+	 		ellipse(xPos, yPos, 5, 5);
+	 	}
+	 	else
+	 	{
+	 		noFill();
+	 		randFill = (int)(Math.random()*3);
+	 		strokeWeight(2);
+	 		if (randFill == 0)
+	 			stroke(27,247, 201);
+	 		else if (randFill == 1)
+	 			stroke(252, 0, 152);
+	 		else 
+	 			stroke(255, 102, 0);
+	 		rect(xPos, yPos, 5, 5);
+		}
  	}
 }    
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -21,10 +21,11 @@ Bacteria [] colony = new Bacteria[NUM_BACTERIA];
  public void setup()   
  {     
  	size(500, 500);
+ 	frameRate(100);
  	//initialize bacteria variables here
  	for (int i = 0; i < colony.length; i++)
  	{
- 		colony[i] = new Bacteria(250, 250);
+ 		colony[i] = new Bacteria(250, 250); //(int)(Math.random()*500), (int)(Math.random()*500)
  	} 
  }
 
@@ -37,11 +38,6 @@ Bacteria [] colony = new Bacteria[NUM_BACTERIA];
  		colony[i].move();
  		colony[i].show();
  	}   
-}
-
-public void mouseClicked()
-{
-	// NUM_BACTERIA = NUM_BACTERIA*2;
 }
 
 class Bacteria    
@@ -60,16 +56,33 @@ class Bacteria
  	}   
  	public void show()
  	{
- 		randFill = (int)(Math.random()*3);
- 		if (randFill == 0)
- 			fill(27,247, 201);
- 		else if (randFill == 1)
- 			fill(252, 0, 152);
- 		else 
- 			fill(255, 102, 0);
- 		noStroke();
- 		// fill(255);
- 		ellipse(xPos, yPos, 5, 5);
+ 		int randShape = (int)(Math.random()*2);
+ 		if (randShape == 0)
+ 		{
+	 		randFill = (int)(Math.random()*3);
+	 		if (randFill == 0)
+	 			fill(27,247, 201);
+	 		else if (randFill == 1)
+	 			fill(252, 0, 152);
+	 		else 
+	 			fill(255, 102, 0);
+	 		noStroke();
+	 		// fill(255);
+	 		ellipse(xPos, yPos, 5, 5);
+	 	}
+	 	else
+	 	{
+	 		noFill();
+	 		randFill = (int)(Math.random()*3);
+	 		strokeWeight(2);
+	 		if (randFill == 0)
+	 			stroke(27,247, 201);
+	 		else if (randFill == 1)
+	 			stroke(252, 0, 152);
+	 		else 
+	 			stroke(255, 102, 0);
+	 		rect(xPos, yPos, 5, 5);
+		}
  	}
 }    
   static public void main(String[] passedArgs) {
